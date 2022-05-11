dead9d930315e2ddb8e1e48b6361272b8ff80181
abiawelewa <101604137+abiawelewa@users.noreply.github.com>
2022-05-10 21:45:20 -0400

    Update iac_usecase_2.tf

--- Labs/usecases/usecase-2/terraform/blue/iac_usecase_2.tf
+++ Labs/usecases/usecase-2/terraform/blue/iac_usecase_2.tf
@@ -55,7 +55,7 @@ variable "RELEASE_VERSION" {
 }
 
 resource "aws_security_group" "basic_http" {
-  name = "sg_flask-usecase2-blue"
+  name = "sg_flask-usecase2-blue1"
   description = "Web Security Group for HTTP"
   vpc_id =  var.VPC
   ingress = [
