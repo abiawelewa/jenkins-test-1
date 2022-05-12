e69cebfa0b35bda01d90396b2994e5374b540725
abiawelewa <101604137+abiawelewa@users.noreply.github.com>
2022-05-11 21:59:11 -0400

    Update iac_usecase_2.tf

--- Labs/usecases/usecase-2/terraform/blue/iac_usecase_2.tf
+++ Labs/usecases/usecase-2/terraform/blue/iac_usecase_2.tf
@@ -50,7 +50,7 @@ variable "EC2_TYPE" {
 
 variable "S3_PATH" {
   type = string
-  default = "s3://devops-usecases2/devops/app"
+  default = "s3://testdataengineeringabi/binary-file/devops/app/usecase2/blue"
   description = "Version to be released"
 }
 
